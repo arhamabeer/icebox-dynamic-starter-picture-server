6de4834a1efca12e2e934cc4e1987f44b32da6d6
arhamabeer <arhamabeerahmed@gmail.com>
2023-06-19 12:39:38 +0500

    Sql Service and Dependency Injection

--- Program.cs
+++ Program.cs
@@ -1,8 +1,16 @@
+using icebox_dynamic_starter_picture_server;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+// ADD SQL SERVICE 
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+// DEPENDENCY INJECTION SERVICE
+builder.Services.AddScoped<IMachineRepository, MachineRepository>();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
