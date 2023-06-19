89e0fa1adeda239c37db8ab8fdefb5d7b0555fd4
arhamabeer <arhamabeerahmed@gmail.com>
2023-06-19 13:23:39 +0500

    hard-coded image return is working

---
+++ Models/Images.cs
@@ -0,0 +1,8 @@
+﻿namespace icebox_dynamic_starter_picture_server.Models
+{
+    public class Images
+    {
+        public string Image { get; set; }
+        public string Type { get; set; }
+    }
+}
--- Models/MachineRepository.cs
+++ Models/MachineRepository.cs
@@ -1,10 +1,39 @@
-﻿namespace icebox_dynamic_starter_picture_server.Models
+﻿using static System.Net.Mime.MediaTypeNames;
+
+namespace icebox_dynamic_starter_picture_server.Models
 {
     public class MachineRepository : IMachineRepository
     {
+        private readonly ApplicationDbContext _context;
+
+        private List<Images> _images;
+
+        public MachineRepository(ApplicationDbContext applicationDbContext) {
+            _images = new List<Images>();
+
+            // Add items to the list
+            _images.Add(new Images { Image = "image1.jpg", Type = "Eid" });
+            _images.Add(new Images { Image = "image2.jpg", Type = "Ramadan" });
+            _images.Add(new Images { Image = "image3.jpg", Type = "New Year" });
+            _images.Add(new Images { Image = "image4.jpg", Type = "Default" });
+
+
+        }
+
         public string GetDisplayBackground(string ip)
         {
-            throw new NotImplementedException();
+            if (string.IsNullOrEmpty(ip))
+            {
+                var img = _images.FirstOrDefault(x => x.Type == "Default");
+                return img.Image;
+            }
+            if(ip == "192.168.0.1")
+            {
+                var img1 = _images.FirstOrDefault(x => x.Type == "Eid");
+                return img1.Image;
+            }
+            var img2 = _images.FirstOrDefault(x => x.Type == "Default");
+            return img2.Image;
         }
     }
 }
