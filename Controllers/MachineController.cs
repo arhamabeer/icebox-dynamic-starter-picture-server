c954b2648adb70a3b8ccd2cf5b310787f5516c95
arhamabeer <arhamabeerahmed@gmail.com>
2023-06-19 13:23:00 +0500

    machine controller

---
+++ Controllers/MachineController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace icebox_dynamic_starter_picture_server.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class MachineController : ControllerBase
+    {
+        private readonly IMachineRepository _machine;
+
+        public MachineController(IMachineRepository machineRepository)
+        {
+            _machine = machineRepository;
+        }
+        [HttpGet("{ip=string}")]
+        public string GetDisplayBackground(string ip)
+        {
+            try
+            {
+                return _machine.GetDisplayBackground(ip);
+            }catch(Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+    }
+}
